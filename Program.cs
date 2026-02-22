689e41f3c6aac3137ee68c6950cce78435f67822
khanh98-24 <nguyenngockhanh982004@gmail.com>
2026-02-22 15:06:22 +0700

    Fix crash

--- Program.cs
+++ Program.cs
@@ -80,5 +80,9 @@ app.MapControllerRoute(
 // =======================
 // RUN
 // =======================
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
