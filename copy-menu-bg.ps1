1acd8a613e912fcad1e2029a7cb03eee77853ada
khanh98-24 <117670185+khanh98-24@users.noreply.github.com>
2026-02-24 00:10:38 +0700

    Delete copy-menu-bg.ps1

--- copy-menu-bg.ps1
+++
@@ -1,2 +0,0 @@
-Copy-Item "C:\Users\nguye\.gemini\antigravity\brain\025d2d85-345c-484b-b3c4-f347c966c12d\uploaded_media_1770827177610.jpg" "d:\WEB Phở\OBeefSoup\wwwroot\images\menu-background.jpg" -Force
-Write-Host "Image copied successfully!"
